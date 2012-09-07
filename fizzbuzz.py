255f705f9bfd31c18aee91d50d10366beb8fd2e3
John Albietz <ydavid365@gmail.com>
2012-09-07 15:03:34 -0700

    tabs to spaces...

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 
-#enter total items to check here. 
+# Enter total items to check here. 
 numtotest = 100
 
-# cannot do this in the range itself.
+# Cannot do this in the range itself.
 numtotest += 1
 
 for i in range(1,numtotest):
-	result = ''
-	if i % 3 == 0:
-		result = 'fizz'
-	if i % 5 == 0:
-		result += 'buzz'
-	if result:
-		print result, i
-	else:
-		print i
+  result = ''
+  if i % 3 == 0:
+    result = 'fizz'
+  if i % 5 == 0:
+    result += 'buzz'
+  if result:
+    print result, i
+  else:
+    print i
